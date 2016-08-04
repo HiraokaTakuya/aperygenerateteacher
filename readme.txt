2f79ee3f47f5b1943a43e25d2dd332f013fb9856
HiraokaTakuya <hiraoka64@gmail.com>
2016-08-04 13:12:55 +0900

    v1.4.0 用にファイルパス変更。

--- readme.txt
+++ readme.txt
@@ -21,6 +21,11 @@ Apery を更に強くする為に、皆様、どうか力を貸して下さい�
 
 更新履歴
 
+2016-08-04  HiraokaTakuya <hiraoka64@gmail.com>
+
+	* v1.4.0
+	評価関数ファイルを更新しました。
+
 2016-07-31  HiraokaTakuya <hiraoka64@gmail.com>
 
 	* v1.3.0
--- src/Form1.cs
+++ src/Form1.cs
@@ -22,7 +22,7 @@ namespace AperyGenerateTeacherGUI
 {
     public partial class Form1 : Form
     {
-        private String version = "1.3.0";
+        private String version = "1.4.0";
         private bool isOlderVersion = false;
         private Label labelThreads;
         private Label labelTeacherNodes;
@@ -109,7 +109,7 @@ namespace AperyGenerateTeacherGUI
             boxLoopLog.Text = "入力不要";
             this.Controls.Add(boxLoopLog);
             labelLog = new Label();
-            labelLog.Text = "ログ";
+            labelLog.Text = "Info";
             labelLog.Height = 15;
             labelLog.Width = this.Width;
             labelLog.Top = boxLoopLog.Bottom + 5;
@@ -389,9 +389,9 @@ namespace AperyGenerateTeacherGUI
             if (!FileIsOK("roots.hcp", 1499386784) ||
                 !FileIsOK("apery.exe", 1363968) ||
                 !FileIsOK("shuffle_hcpe.exe", 864768) ||
-                !FileIsOK("20160730\\KPP_synthesized.bin", 776402496) ||
-                !FileIsOK("20160730\\KKP_synthesized.bin", 81251424) ||
-                !FileIsOK("20160730\\KK_synthesized.bin", 52488))
+                !FileIsOK("20160803\\KPP_synthesized.bin", 776402496) ||
+                !FileIsOK("20160803\\KKP_synthesized.bin", 81251424) ||
+                !FileIsOK("20160803\\KK_synthesized.bin", 52488))
             {
                 return;
             }
