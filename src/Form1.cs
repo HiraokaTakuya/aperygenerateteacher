36fc6bef80959062bebd186ced58135b4faa826c
HiraokaTakuya <hiraoka64@gmail.com>
2016-08-10 08:49:10 +0900

    v1.5.0 用にファイルパス変更。

--- src/Form1.cs
+++ src/Form1.cs
@@ -22,7 +22,7 @@ namespace AperyGenerateTeacherGUI
 {
     public partial class Form1 : Form
     {
-        private String version = "1.4.0";
+        private String version = "1.5.0";
         private bool isOlderVersion = false;
         private Label labelThreads;
         private Label labelTeacherNodes;
@@ -389,9 +389,9 @@ namespace AperyGenerateTeacherGUI
             if (!FileIsOK("roots.hcp", 1499386784) ||
                 !FileIsOK("apery.exe", 1363968) ||
                 !FileIsOK("shuffle_hcpe.exe", 864768) ||
-                !FileIsOK("20160803\\KPP_synthesized.bin", 776402496) ||
-                !FileIsOK("20160803\\KKP_synthesized.bin", 81251424) ||
-                !FileIsOK("20160803\\KK_synthesized.bin", 52488))
+                !FileIsOK("20160810\\KPP_synthesized.bin", 776402496) ||
+                !FileIsOK("20160810\\KKP_synthesized.bin", 81251424) ||
+                !FileIsOK("20160810\\KK_synthesized.bin", 52488))
             {
                 return;
             }
@@ -410,7 +410,7 @@ namespace AperyGenerateTeacherGUI
 
             this.thread = new Thread(new ThreadStart(this.ThreadProcSafe));
 
-	        this.thread.Start();
+            this.thread.Start();
             SetButton(false);
         }
         private static void CompressFile(string inFile, string outFile)
